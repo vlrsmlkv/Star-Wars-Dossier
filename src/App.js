8fba8905a1d22ad6372bc347b2aecd2f34c2f9c4
OUghTMS <misha.saan@gmail.com>
2021-06-06 13:49:03 +0300

    build: add rsuite design lib

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { Button } from 'rsuite';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       Hello! 
+      <Button appearance="primary"> Hello world </Button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'rsuite/dist/styles/rsuite-default.css';
+
 import App from './App';
 
 ReactDOM.render(
